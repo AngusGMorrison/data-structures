ebc57a7888d98e4871d058ccae12bd2579392a9b
Angus Morrison <contact@angus-morrison.com>
2020-01-12 11:49:16 +0000

    Wrote tests for insertion

--- avl_tree/avl_tree.rb
+++ avl_tree/avl_tree.rb
@@ -1,6 +1,8 @@
 require_relative './node'
 
 class AVLTree
+  attr_reader :root
+
   def initialize
     @root = nil
   end
@@ -28,43 +30,39 @@ class AVLTree
   private def balance(node)
     set_height(node)
     if left_heavy?(node)
-      if node.left.right.height > node.left.left.height
-        return rotate_left_right(node)
-      else
-        return rotate_right(node)
-      end
+      return long_right_branch?(node) ? rotate_left_right(node) : rotate_right(node)
     elsif right_heavy?(node)
-      if node.right.left.height > node.left.left.height
-        return rotate_right_left(node)
-      else
-        return rotate_left(node)
-      end
+      return long_left_branch?(node) ? rotate_right_left(node) : rotate_left(node)
     end
-
     node
   end
 
   private def left_heavy?(node)
-    lh = node.left ? node.left.height : 0
-    rh = node.right ? node.right.height : 0
-    lh - rh == 2
+    height(node.left) - height(node.right) == 2
   end
 
   private def right_heavy?(node)
-    lh = node.left ? node.left.height : 0
-    rh = node.right ? node.right.height : 0
-    rh - lh == 2
+    height(node.right) - height(node.left) == 2
   end
 
-  private def rotate_right(original_parent_node)
-    new_parent = original_parent_node.left
-    original_parent_node.left = new_parent.right
-    new_parent.right = original_parent_node
+  private def long_right_branch?(node)
+    return false unless node.left
+    height(node.left.right) > height(node.left.left)
+  end
 
-    set_height(original_parent_node)
-    set_height(new_parent)
+  private def long_left_branch?(node)
+    return false unless node.right
+    height(node.right.left) > height(node.right.right)
+  end
 
-    new_parent
+  private def rotate_left_right(original_parent_node)
+    original_parent_node.left = rotate_left(original_parent_node.left)
+    return rotate_right(original_parent_node)
+  end
+
+  private def rotate_right_left(original_parent_node)
+    original_parent_node.right = rotate_right(original_parent_node.right)
+    return rotate_left(original_parent_node)
   end
 
   private def rotate_left(original_parent_node)
@@ -78,25 +76,51 @@ class AVLTree
     new_parent
   end
 
-  private def rotate_left_right(original_parent_node)
-    original_parent_node.left = rotate_left(original_parent_node.left)
-    return rotate_right(original_parent_node)
-  end
+  private def rotate_right(original_parent_node)
+    new_parent = original_parent_node.left
+    original_parent_node.left = new_parent.right
+    new_parent.right = original_parent_node
 
-  private def rotate_right_left(original_parent_node)
-    original_parent_node.right = rotate_right(original_parent_node.right)
-    return rotate_left(original_parent_node)
+    set_height(original_parent_node)
+    set_height(new_parent)
+
+    new_parent
   end
 
   private def set_height(node)
     # O(1)
-    lh = node.left ? node.left.height : 0
-    rh = node.right ? node.right.height : 0
+    lh = height(node.left)
+    rh = height(node.right)
     max = lh > rh ? lh : rh
     node.height = max + 1
   end
 
-  private def remove(key)
-    search(key).deleted = true
+  private def height(node)
+    return 0 unless node
+    node.height
+  end
+
+  def remove(key)
+    # O(log2n)
+    node = search(key)
+    node.deleted = true if node
+    node
+  end
+
+  def search(key)
+    # O(log2n)
+    node = search_from_node(@root, key)
+    node unless node.deleted
+  end
+
+  def search_from_node(node, key)
+    # O(log2n)
+    return nil unless node
+    if key < node.key
+      return search_from_node(node.left, key)
+    elsif key > node.key
+      return search_from_node(node.right, key)
+    end
+    node
   end
 end
---
+++ avl_tree/spec/avl_tree_spec.rb
@@ -0,0 +1,73 @@
+require 'rspec'
+require_relative '../avl_tree';
+
+RSpec.describe AVLTree do
+
+  context 'initialize' do
+    it 'initializes an AVL tree with root nil' do
+      tree = AVLTree.new
+      expect(tree.root).to be(nil);
+    end
+  end
+
+  context 'insert' do
+    left_heavy_tree = AVLTree.new
+
+    it 'makes the first node inserted the root node' do
+      left_heavy_tree.insert(6, "six")
+      expect(left_heavy_tree.root.key).to be(6);
+    end
+
+    it 'inserts lower nodes to the left of the root node' do
+      left_heavy_tree.insert(3, "three")
+      expect(left_heavy_tree.root.left.key).to be(3)
+    end
+
+    it 'inserts higher nodes to the right of the root node' do
+      left_heavy_tree.insert(9, "nine")
+      expect(left_heavy_tree.root.right.key).to be(9)
+    end
+
+    it 'right-rotates the tree when the left branch grows too long with a long left tail' do
+      left_heavy_tree.insert(2, "two")
+      left_heavy_tree.insert(1, "one")
+      expect(left_heavy_tree.root.left.key).to be(2)
+      expect(left_heavy_tree.root.left.right.key).to be(3)
+      expect(left_heavy_tree.root.left.left.key).to be(1)
+    end
+
+    it 'left-right rotates the tree when the left branch grows too long with a long right tail' do
+      left_heavy_tree.insert(4, "four")
+      expect(left_heavy_tree.root.key).to be(3)
+      expect(left_heavy_tree.root.right.key).to be(6)
+      expect(left_heavy_tree.root.right.left.key).to be(4)
+      expect(left_heavy_tree.root.right.right.key).to be(9)
+      expect(left_heavy_tree.root.left.key).to be(2)
+      expect(left_heavy_tree.root.left.left.key).to be(1)
+    end
+
+    right_heavy_tree = AVLTree.new
+    right_heavy_tree.insert(6, "six")
+    right_heavy_tree.insert(3, "three")
+    right_heavy_tree.insert(9, "nine")
+
+    it 'left-rotates the tree when the right branch grows too long with a long right tail' do
+      right_heavy_tree.insert(11, "eleven")
+      right_heavy_tree.insert(13, "thirteen")
+      expect(right_heavy_tree.root.right.key).to be(11)
+      expect(right_heavy_tree.root.right.right.key).to be(13)
+      expect(right_heavy_tree.root.right.left.key).to be(9)
+    end
+
+    it 'right-left rotates the tree when the right branch grows too long with a long left tail' do
+      right_heavy_tree.insert(8, "eight")
+      expect(right_heavy_tree.root.key).to be(9)
+      expect(right_heavy_tree.root.right.key).to be(11)
+      expect(right_heavy_tree.root.right.right.key).to be(13)
+      expect(right_heavy_tree.root.left.key).to be(6)
+      expect(right_heavy_tree.root.left.left.key).to be(3)
+      expect(right_heavy_tree.root.left.right.key).to be(8)
+    end
+  end
+
+end
