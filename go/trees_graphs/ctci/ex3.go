174c0b27579460f5e8308ae8483246de9bba8195
Angus Morrison <contact@angus-morrison.com>
2021-04-29 20:10:16 +0100

    Add BFS implementation to ctci ch4ex2

--- go/trees_graphs/ctci/ex3.go
+++ go/trees_graphs/ctci/ex3.go
@@ -4,11 +4,12 @@ package ctci
 // linked list of all the nodes at each depth (e.g. if you have a tree with
 // depth D, you'll have D linked lists).
 
-// GenerateDepthLists returns a map of depths to linked lists containing all
-// nodes in the input tree at that depth. The lists are built recursively by
-// incrementing the depth as it is passed down the call stack. Takes O(n) time
-// and O(n) space – we must visit each node and we must store each tree node in
-// a list node.
+// GenerateDepthListsRecursive returns a map of depths to linked lists
+// containing all nodes in the input tree at that depth. The lists are built
+// recursively by incrementing the depth as it is passed down the call stack.
+//
+// Takes O(n) time and O(n) space – we must visit each node and we must store
+// each tree node in a list node.
 //
 // Assumptions:
 //   * A map of linked lists is an acceptable return value. Confirm this with
@@ -17,7 +18,7 @@ package ctci
 //     do not have next fields.
 //   * Root node is at depth 1.
 //   * The order in which nodes are appended/prepended to the linked lists is unimportant.
-func GenerateDepthLists(root *BinaryTreeNode) map[int]*BinaryTreeListNode {
+func GenerateDepthListsRecursive(root *BinaryTreeNode) map[int]*BinaryTreeListNode {
 	if root == nil {
 		return nil
 	}
@@ -27,6 +28,42 @@ func GenerateDepthLists(root *BinaryTreeNode) map[int]*BinaryTreeListNode {
 		depthLists[depth] = NewBinaryTreeListNode(n, depthLists[depth])
 	}
 	root.MapWithDepth(1, listBuilder)
+
+	return depthLists
+}
+
+// GenerateDepthListsBFS returns a map of depths to linked lists containing all
+// nodes in the input tree at that depth. The lists are built by breadth-first
+// search (BFS), incrementing the depth each time the search moves one level
+// deeper.
+// Takes O(n) time and O(n) space. Although BFS doesn't require log n
+// recursive calls on the stack, it must still create a linked list node for
+// each node in the input tree.
+func GenerateDepthListsBFS(root *BinaryTreeNode) map[int]*BinaryTreeListNode {
+	if root == nil {
+		return nil
+	}
+
+	depthLists := make(map[int]*BinaryTreeListNode)
+	worklist := []*BinaryTreeNode{root}
+	var depth int
+
+	for len(worklist) > 0 {
+		nodes := worklist
+		worklist = nil
+		depth += 1
+
+		for _, n := range nodes {
+			depthLists[depth] = NewBinaryTreeListNode(n, depthLists[depth])
+
+			if n.left != nil {
+				worklist = append(worklist, n.left)
+			}
+			if n.right != nil {
+				worklist = append(worklist, n.right)
+			}
+		}
+	}
 
 	return depthLists
 }
--- go/trees_graphs/ctci/ex3_test.go
+++ go/trees_graphs/ctci/ex3_test.go
@@ -6,9 +6,9 @@ import (
 	"testing"
 )
 
-func TestGenerateDepthLists(t *testing.T) {
+func TestGenerateDepthListsRecursive(t *testing.T) {
 	t.Run("nil tree", func(t *testing.T) {
-		got := GenerateDepthLists(nil)
+		got := GenerateDepthListsRecursive(nil)
 		if got != nil {
 			t.Errorf("want nil, got %v", got)
 		}
@@ -19,7 +19,48 @@ func TestGenerateDepthLists(t *testing.T) {
 		root.MassInsert(30, 10, 40, 70, 60, 80)
 
 		wantNLists := 3
-		got := GenerateDepthLists(root)
+		got := GenerateDepthListsRecursive(root)
+		if len(got) != wantNLists {
+			t.Errorf("want %d lists, got %d:\n%s", wantNLists, len(got), depthListsToString(got))
+		}
+
+		depth1List := got[1]
+		if depth1List.treeNode != root {
+			t.Errorf("want list at depth 1 to contain the root node %+v, got %+v:\n%s",
+				root, depth1List.treeNode, depth1List)
+		}
+
+		wantDepth3Elements := []*BinaryTreeNode{
+			root.right.right,
+			root.right.left,
+			root.left.right,
+			root.left.left,
+		}
+
+		depth3List := got[3]
+		for cur, i := depth3List, 0; cur != nil; cur, i = cur.next, i+1 {
+			if cur.treeNode != wantDepth3Elements[i] {
+				t.Errorf("want list at depth 3 to contain node %+v at position %d, got %+v\n%s",
+					wantDepth3Elements[i], i, cur, depth3List)
+			}
+		}
+	})
+}
+
+func TestGenerateDepthListsBFS(t *testing.T) {
+	t.Run("nil tree", func(t *testing.T) {
+		got := GenerateDepthListsBFS(nil)
+		if got != nil {
+			t.Errorf("want nil, got %v", got)
+		}
+	})
+
+	t.Run("non-nil tree", func(t *testing.T) {
+		root := NewBinaryTreeNode(50, nil, nil)
+		root.MassInsert(30, 10, 40, 70, 60, 80)
+
+		wantNLists := 3
+		got := GenerateDepthListsBFS(root)
 		if len(got) != wantNLists {
 			t.Errorf("want %d lists, got %d:\n%s", wantNLists, len(got), depthListsToString(got))
 		}
